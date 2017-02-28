2f71b133fc1ee8c15a6921e76cd474a54f920412
Mads Gram <mgmadsgram@gmail.com>
2017-02-28 23:15:17 +0100

    Update run_code to take all the contracts in contracts.json and define them in the geth session

--- tools/interpret_contracts_json.py
+++ tools/interpret_contracts_json.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # sys.argv[1] is outdir
-# sys.argv[2] is name of the deployed contract (file name and constructor name)
-# sys.argv[3] is token symbol
 # DEVFIX: Add better error handling here!
 
 import json
 import sys
 
-json_fn   =sys.argv[1] + "contracts.json"
-abi_def_fn=sys.argv[1] + sys.argv[2] + ".sol:" + sys.argv[2] + ".abi"
+json_fn = sys.argv[1] + "contracts.json"
 
 def get_address():
     with open(json_fn, "r") as fh:
         data = json.load(fh)
+        finalResult = []
+        i=0
+
         for item in data:
-            target = filter( lambda x: x["name"] == "tokenSymbol" and x["value"] == sys.argv[3], item["args"] )
-            if (len(target) == 1):
-                print "address: " + item["address"]
-                return
-            if (len(target) > 1):
-                print "ambiguous argument names recorded in " + json_fn
-                sys.exit(1)
-        print "ERROR: data for deployed contract " + sys.argv[2] + ", " + sys.argv[3] + " not found!"
-        sys.exit(1)
+            finalResult.append([])
 
-def get_abi_definition():
-    with open(abi_def_fn) as fh:
-        print "ABI_DEF: " + fh.read()
+            finalResult[i].append(item["address"])
+            finalResult[i].append(item["name"])
 
-get_address()
-get_abi_definition()
+            ts =  filter(lambda x:x["name"] == "tokenSymbol", item["args"])
+
+            if len(ts) == 1:
+                finalResult[i].append(ts[0]["value"])
+            i+=1
+
+        for item in finalResult:
+            print " ".join(item)
+
+if __name__ == '__main__':
+    get_address()
