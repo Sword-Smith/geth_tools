7b66d6b6b92c0711a40c8cccb360cf837f600605
Mads Gram <mgmadsgram@gmail.com>
2017-03-09 02:24:14 +0100

    Integration test for Tmc4 and TC0

--- tools/find_any_address_constructor_args.py
+++ tools/find_any_address_constructor_args.py
@@ -2,18 +2,29 @@
  # -*- coding: utf-8 -*-
 import json
 import sys
+import os
 
-with open(sys.argv[1]+"/contracts.json") as data_file:
-    data = json.load(data_file)
-    contract_name = sys.argv[2][9:].split('_')
+def main(outdir, contractName, fromCmd):
+    with open(outdir+"/contracts.json") as data_file:
+        data = json.load(data_file)
+        contract_name = contractName[9:].split('_')
 
-    for contract in data:
-        if ( contract_name[0] == contract["name"]):
-            mcs= filter(lambda x:x["name"]=="tokenSymbol", contract["args"])
-            if len(mcs) == 1:
-                if (mcs[0]["value"] == contract_name[1]):
-                    print contract["address"]
-            elif len(mcs) == 0:
-                print contract["address"]
-            else: 
-                sys.exit(1)
+        for contract in data:
+            if ( contract_name[0] == contract["name"]):
+                mcs= filter(lambda x:x["name"]=="tokenSymbol", contract["args"])
+                if len(mcs) == 1:
+                    if (mcs[0]["value"] == contract_name[1]):
+                        if (fromCmd):
+                            print contract["address"]
+                        else:
+                            return contract["address"]
+                elif len(mcs) == 0:
+                        if (fromCmd):
+                            print contract["address"]
+                        else:
+                            return contract["address"]
+                else:
+                    sys.exit(1)
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], True)
