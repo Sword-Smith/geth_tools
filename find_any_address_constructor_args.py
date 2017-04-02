c3e1962c0ecb33acf88c4f05bddccea15940a73c
Mads Gram <mgmadsgram@gmail.com>
2017-04-02 17:42:00 +0200

    Get token addresses based on keywords for tokens and addresses

--- find_any_address_constructor_args.py
+++ find_any_address_constructor_args.py
@@ -5,26 +5,29 @@ import sys
 import os
 
 def main(outdir, contractName, fromCmd):
-    with open(outdir+"/contracts.json") as data_file:
-        data = json.load(data_file)
-        contract_name = contractName[9:].split('_')
+    if os.path.isfile(outdir+"/contracts.json"):
+        with open(outdir+"/contracts.json") as data_file:
+            data = json.load(data_file)
+            contract_name = contractName[9:].split('_')
 
-        for contract in data:
-            if ( contract_name[0] == contract["name"]):
-                mcs= filter(lambda x:x["name"]=="tokenSymbol", contract["args"])
-                if len(mcs) == 1:
-                    if (mcs[0]["value"] == contract_name[1]):
-                        if (fromCmd):
-                            print contract["address"]
-                        else:
-                            return contract["address"]
-                elif len(mcs) == 0:
-                        if (fromCmd):
-                            print contract["address"]
-                        else:
-                            return contract["address"]
-                else:
-                    sys.exit(1)
+            for contract in data:
+                if ( contract_name[0] == contract["name"]):
+                    mcs= filter(lambda x:x["name"]=="tokenSymbol", contract["args"])
+                    if len(mcs) == 1:
+                        if (mcs[0]["value"] == contract_name[1]):
+                            if (fromCmd):
+                                print contract["address"]
+                            else:
+                                return contract["address"]
+                    elif len(mcs) == 0:
+                            if (fromCmd):
+                                print contract["address"]
+                            else:
+                                return contract["address"]
+                    else:
+                        sys.exit(1)
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], True)
