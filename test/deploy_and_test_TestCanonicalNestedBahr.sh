5556050e4ecdac4b530fbcbf3ffa892e97e93c16
Thor <thorkilk@gmail.com>
2017-04-29 18:47:16 +0200

    Add missing plings to bash script for canonical nested if-within contract

--- test/deploy_and_test_TestCanonicalNestedBahr.sh
+++ test/deploy_and_test_TestCanonicalNestedBahr.sh
@@ -9,9 +9,9 @@ con_exe_path="./test/contract_execution/con_exec_TestCanonicalNestedBahr.js"
 
 rm -rf $outdir
 ./compile_and_deploy $outdir $tmc_path 1000 "'EUR'" 0 "'EUR'"
-./compile_and_deploy $outdir $DF_path0 "ZERO"
-./compile_and_deploy $outdir $DF_path1 "ONE"
-./compile_and_deploy $outdir $DF_path2 "TWO"
+./compile_and_deploy $outdir $DF_path0 "'ZERO'"
+./compile_and_deploy $outdir $DF_path1 "'ONE'"
+./compile_and_deploy $outdir $DF_path2 "'TWO'"
 ./compile_and_deploy $outdir $TC_path
 ./run_code $outdir $con_exe_path
 sleep 120
