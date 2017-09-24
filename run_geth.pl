4cf17e33b5f73c598655672e5000af49b9f5a631
Thorkil Værge <thorkilk@gmail.com>
2017-09-24 17:47:57 +0200

    Use localhost instead of own IP to fix Truffle compliance
    When starting an instance of geth through run_geth.pl, localhost is now
    used instead of the computer's own IP address. This was done to make
    the running instance of geth compliant with truffle.

--- run_geth.pl
+++ run_geth.pl
@@ -2,7 +2,7 @@
 use v5.22;
 
 use File::HomeDir;
-use Net::Address::IP::Local;
+#use Net::Address::IP::Local;
 
 my $system_platform = `uname`; # Runs uname and stores the output in var
 chomp $system_platform;
@@ -17,7 +17,9 @@ if ( $system_platform eq "Linux" ){
     die "The system platform is not Linux or Darwin, we only support one of these";
 }
 
-my $own_ip = Net::Address::IP::Local->public();
+# Comment out own_ip to fix Truffle compliance
+# Truffle could prob. also be fixed to work with this, but this seemed easier.
+#my $own_ip = Net::Address::IP::Local->public();
 
 # Open geth and mine, pipe output to /out/error.log
-system("geth --fast --cache 512 --ipcpath $ipcpath --networkid 42 --port 30301 --rpc --rpcaddr $own_ip --rpcport 30302 --datadir ./.ethereum_testserver_data --jspath ./scripts/ --mine --minerthreads=1 console 2>> ./out/error.log");
+system("geth --fast --cache 512 --ipcpath $ipcpath --networkid 42 --port 30301 --rpc --rpcport 30302 --rpcaddr localhost --rpccorsdomain '*' --rpcapi 'eth,net,web3' --datadir ./.ethereum_testserver_data --jspath ./scripts/ --mine --minerthreads=1 console 2>> ./out/error.log");
