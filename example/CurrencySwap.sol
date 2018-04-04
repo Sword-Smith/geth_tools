5aca3d57361c21616ee5d53db044ed1b9db4ab78
Thorkil Værge <thorkilk@gmail.com>
2018-04-05 00:58:28 +0200

    Add public accessor keywords to Solidity function

--- example/CurrencySwap.sol
+++ example/CurrencySwap.sol
@@ -9,16 +9,16 @@ contract CurrencySwap {
   address addrA;
   address addrB;
 
-  function CurrencySwap(){
+  function CurrencySwap() public {
     tokenContractA = Tmc4(_address_Tmc4_A);
     tokenContractB = Tmc4(_address_Tmc4_B);
-    dataFeed       = DataFeed0(_address_DataFeed0_A);
+    dataFeed       = DataFeed0(_address_DataFeed0_);
     addrA          = _address_my_;
     addrB          = 0x0123456789012345678901234567890123456789;
   }
 
   // For this to work, the balances to be non-negative after this execution
-  function execute(){
+  function execute() public {
     uint256 value = dataFeed.get(0);
     tokenContractA.transferFrom( addrA, addrB, 1 );
     tokenContractB.transferFrom( addrA, addrB, value );
