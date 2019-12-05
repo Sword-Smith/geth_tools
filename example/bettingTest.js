943f398908a960b3667b02bfd0d51700a9797d86
Thorkil Værge <thorkilk@gmail.com>
2019-12-05 16:21:44 +0100

    Beautify output of betting example

--- example/bettingTest.js
+++ example/bettingTest.js
@@ -1,56 +1,63 @@
 web3.eth.defaultAccount = web3.eth.accounts[0];
-me = web3.eth.defaultAccount;
+me = web3.eth.accounts[0];
 other = web3.eth.accounts[1];
+console.log(""); // Print an extra line to make output look nicer
 
-console.log("Transferring ether from me to other.")
 var t0 = web3.eth.sendTransaction({ from: me, to: other, value: web3.toWei(1, "ether") });
 var t1 = web3.eth.getTransaction(t0);
 while(t1.blockNumber === null) {
     t1 = web3.eth.getTransaction(t0);
 }
 
-console.log("Transferring DKK from me to other.")
-var t0 = Erc20_DKK.transfer(other, 10000);
+var t0 = Erc20_CHF.transfer(other, 10000);
 var t1 = web3.eth.getTransaction(t0);
 while(t1.blockNumber === null) {
     t1 = web3.eth.getTransaction(t0);
 }
 
-var dataFeedValue = 1;
+var dataFeedValue = 0;
 var BettingAddress = BettingExample2_.address;
-var valueBeforeActivate = Erc20_DKK.balanceOf(me);
+var valueBeforeActivate = Erc20_CHF.balanceOf(me);
 
 
 function printBalances() {
-    console.log("Ether balance (me): " + web3.eth.getBalance(me));
-    console.log("Ether balance (other): " + web3.eth.getBalance(other));
-
-    var my_balance = Erc20_DKK.balanceOf(me);
-    console.log("My balance on token contract DKK is: " + my_balance);
-    var other_balance = Erc20_DKK.balanceOf(other);
-    console.log("Other balance on token contract DKK is: " + other_balance);
-    var betting_balance = Erc20_DKK.balanceOf(BettingAddress);
-    console.log("DC balance on token contract DKK is: " + betting_balance);
-}
-
-printBalances();
-
-function formatError(actual, expected, reason) {
-        throw "\nActual: " + actual.toString() +
-              "\nExpected: " + expected.toString() +
-              (reason === "" ? "" : reason);
+    var my_balance = Erc20_CHF.balanceOf(me);
+    console.log("My balance on token contract CHF is: " + my_balance);
+    var other_balance = Erc20_CHF.balanceOf(other);
+    console.log("Other balance on token contract CHF is: " + other_balance);
+    var betting_balance = Erc20_CHF.balanceOf(BettingAddress);
+    console.log("Derivative contract balance on token contract CHF is: " + betting_balance);
+    console.log("\n\n");
 }
 
 function assertEquals(actual, expected, reason) {
-    if (actual != expected) {
+    if (actual !== expected) {
         formatError(actual, expected, reason);
+    } else {
+        console.log(reason + " ... OK");
     }
 }
 
 function assertNotEquals(actual, expected, reason) {
-    if (actual == expected) {
+    if (actual === expected) {
         formatError(actual, expected, reason);
+    } else {
+        console.log(reason + " ... OK");
     }
+}
+
+function formatError(actual, expected, reason) {
+        throw "\nActual: " + actual.toString() +
+              "\nExpected: " + expected.toString() +
+              "\n" +
+              (reason === "" ? "" : reason + " ... FAIL");
+}
+
+function sleep(ms) {
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < ms);
 }
 
 console.log("Setting data feed to " + dataFeedValue);
@@ -60,13 +67,13 @@ while(t1.blockNumber === null){
     t1 = web3.eth.getTransaction(df);
 }
 
-console.log("********* Before bet *********");
+console.log("\n\n\n********* Before bet *********");
 printBalances();
 
 var validApproveAmount = 10000;
-console.log("Approve with sufficient amount (" + validApproveAmount + ")")
+console.log("Approve with sufficient amount, " + validApproveAmount);
 
-b_A = Erc20_DKK.approve( BettingAddress, validApproveAmount, {from: me, gas: 3000000} );
+b_A = Erc20_CHF.approve( BettingAddress, validApproveAmount, {from: me, gas: 3000000} );
 t0_A = web3.eth.getTransaction(b_A);
 while(t0_A.blockNumber === null){
     t0_A = web3.eth.getTransaction(b_A);
@@ -74,21 +81,21 @@ while(t0_A.blockNumber === null){
 console.log("Done approving.")
 
 assertEquals(
-    Erc20_DKK.allowance(me, BettingAddress),
+    Erc20_CHF.allowance(me, BettingAddress).toNumber(),
     validApproveAmount,
-    "\nReason: Check allowance after correct approve call..");
+    "Check allowance after correct approve call..");
 
 
-b_A = Erc20_DKK.approve( BettingAddress, validApproveAmount, {from: other, gas: 3000000} );
+b_A = Erc20_CHF.approve( BettingAddress, validApproveAmount, {from: other, gas: 3000000} );
 t0_A = web3.eth.getTransaction(b_A);
 while(t0_A.blockNumber === null){
     t0_A = web3.eth.getTransaction(b_A);
 }
 
 assertEquals(
-    Erc20_DKK.allowance(other, BettingAddress),
+    Erc20_CHF.allowance(other, BettingAddress).toNumber(),
     validApproveAmount,
-    "\nReason: Check allowance after correct approve call..");
+    "Check allowance after correct approve call..");
 
 
 // Call to activate
@@ -98,49 +105,50 @@ while(t0_A.blockNumber === null){
     t0_A = web3.eth.getTransaction(b_A);
 }
 
+console.log("\n\n\n********* Funds transferred to blockchain *********");
 printBalances();
 
 assertEquals(
-    Erc20_DKK.balanceOf(BettingAddress),
+    Erc20_CHF.balanceOf(BettingAddress).toNumber(),
     2 * validApproveAmount,
-    "\nReason: Check DC balance after approve and activate.");
+    "Check Derivative contract balance after approve and activate.");
 
-// Assert that the money has been moved from me and other to DC
+// Assert that the money has been moved from me and other to Derivative contract
 // Check my balance change
 assertEquals(
-    Erc20_DKK.balanceOf(me),
+    Erc20_CHF.balanceOf(me).toNumber(),
     valueBeforeActivate - validApproveAmount,
-    "\nReason: Check my balance after activate (me)");
+    "Check my balance after activate");
 
 assertEquals(
-    Erc20_DKK.balanceOf(other),
-    0,
-    "\nReason: Check my balance after activate (other)");
+    Erc20_CHF.balanceOf(other).toNumber(),
+    valueBeforeActivate - validApproveAmount,
+    "Check other balance after activate");
 
-// Execute the European option contract
+// Execute the simple betting contract
 cs  = BettingExample2_.execute({from: me, gas: 3000000});
 tcs = web3.eth.getTransaction(cs);
 while(tcs.blockNumber === null){
     tcs = web3.eth.getTransaction(cs);
 }
 
-// Assert that DC has no money left
+// Assert that Derivative contract has no money left
 assertEquals(
-    Erc20_DKK.balanceOf(BettingAddress),
+    Erc20_CHF.balanceOf(BettingAddress).toNumber(),
     0,
-    "\nReason: Check DC balance after execute");
+    "Check Derivative contract balance after execute");
 
 //Assert that the right amount was sent back to me after execute
 assertEquals(
-    Erc20_DKK.balanceOf(me),
-    980000,
-    "\nReason: Check my balance after execute");
+    Erc20_CHF.balanceOf(me).toNumber(),
+    2 * validApproveAmount,
+    "Check my balance after execute");
 
 // Assert that recipient has received the correct amount
 assertEquals(
-    Erc20_DKK.balanceOf(other),
-    20000,
-    "\nReason: Check other balance after execute");
+    Erc20_CHF.balanceOf(other).toNumber(),
+    valueBeforeActivate - validApproveAmount,
+    "Check other balance after execute");
 
+console.log("\n\n\n********* Bet has been settled *********");
 printBalances();
-console.log("wat.");
