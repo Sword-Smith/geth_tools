e5f6afa70e013f5e21054dfdb39ac50f113633f2
Thorkil Vaerge <thorkilk@gmail.com>
2020-12-12 19:52:59 +0100

    Add more tests to TransferChecks

--- TransferChecks/TransferChecks.js
+++ TransferChecks/TransferChecks.js
@@ -32,3 +32,46 @@ assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other should have rece
 assertEquals(contract.balanceOf(other, 1).toNumber(), 0, "Other should not have any of PT 1.");
 
 fail(do_transfer(contract, me, "0x0000000000000000000000000000000000000000", 0, 1), "Fail when _to is 0 (ERC-1155 requirement).");
+
+// Transfer PT1 to other
+succ(do_transfer(contract, me, other, 1, 4));
+
+assertEquals(contract.balanceOf(me, 0).toNumber(), 1, "I should still have transferred 4 of PT 0 to other.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 1, "I should also have transferred 4 of PT 1 to other.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other should still have received 4 of PT 0.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 4, "Other should also have received 4 of PT 1.");
+
+// Disallow transfer of 2 when balance is 1
+fail(do_transfer(contract, me, other, 0, 2), "Disallow transfer of PT0 when amount exceeds balance");
+fail(do_transfer(contract, me, other, 1, 2), "Disallow transfer of PT1 when amount exceeds balance");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 1, "PT0 balance is 1 after failed transfers.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 1, "PT1 balance is 1 after failed transfers.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other's PT0 balance is 4 after failed transfers.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 4, "Other's PT1 balance is 4 after failed transfers.");
+
+// Transfer to self should be a nop
+succ(do_transfer(contract, me, me, 0, 1));
+assertEquals(contract.balanceOf(me, 0).toNumber(), 1, "Transferring PT0 to self is NOP for PT0 balance.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 1, "Transferring PT0 to self is NOP for PT1 balance.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other's PT0 balance is unaffected by NOP.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 4, "Other's PT1 balance is unaffected by NOP.");
+
+// Allow transfer of 1 more token for both PTs
+succ(do_transfer(contract, me, other, 0, 1));
+assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "Own PT0 balance is now 0.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 1, "Own PT0 balance is still 1.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 5, "Other's PT0 balance is now 5.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 4, "Other's PT1 balance is still 4.");
+
+succ(do_transfer(contract, me, other, 1, 1));
+assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "Own PT0 balance is still 0.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 0, "Own PT0 balance is now 0.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 5, "Other's PT0 balance is still 5.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 5, "Other's PT1 balance is now 5.");
+
+// Disallow any more transfers
+fail(do_transfer(contract, me, other, 0, 1));
+assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "Own PT0 balance still empty.");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 0, "Own PT1 balance still empty.");
+assertEquals(contract.balanceOf(other, 0).toNumber(), 5, "Other's PT0 balance is unaffected by failed transfer.");
+assertEquals(contract.balanceOf(other, 1).toNumber(), 5, "Other's PT1 balance is unaffected by failed transfer");
