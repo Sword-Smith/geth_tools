3d342110cf523add7beab86151ba38014194f32f
Thorkil Vaerge <thorkilvaerge@bitgo.com>
2020-12-12 11:28:24 +0100

    Reduce output from each test to give better overview

--- BalanceChecks/run_BalanceChecks.sh
+++ BalanceChecks/run_BalanceChecks.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e # Halt on error
 
 rm -f out/contracts.json
-./compile_and_deploy.pl out example/Erc20.sol "'SettledAsset'" "'CHF'" 0 20000 &&
-./compile_and_deploy.pl out example/DataFeed0.sol &&
-./compile_and_deploy.pl out example/DataFeed1.sol &&
-./compile_and_deploy.pl out BalanceChecks/BalanceChecks.dag &&
+./compile_and_deploy.pl out example/Erc20.sol "'SettledAsset'" "'CHF'" 0 20000 > /dev/null
+./compile_and_deploy.pl out example/DataFeed0.sol > /dev/null
+./compile_and_deploy.pl out example/DataFeed1.sol > /dev/null
+./compile_and_deploy.pl out BalanceChecks/BalanceChecks.dag > /dev/null
 # prepend lib to every test-set
-cat lib.js BalanceChecks/BalanceChecks.js > out/test.js &&
+cat lib.js BalanceChecks/BalanceChecks.js > out/test.js
 ./run_code.pl out out/test.js
