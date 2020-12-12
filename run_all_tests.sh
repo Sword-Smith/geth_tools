3501383256ad6c834495073441891a5ef1f53bab
Thorkil Vaerge <thorkilvaerge@bitgo.com>
2020-12-12 11:07:39 +0100

    Add script to run all tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-./run_SafeAdd.sh &&
-./run_SafeSub.sh &&
-./run_SafeMul.sh &&
-./run_SafeDiv.sh &&
+# ./run_SafeAdd.sh &&
+# ./run_SafeSub.sh &&
+# ./run_SafeMul.sh &&
+# ./run_SafeDiv.sh &&
 ./run_ActivationChecks.sh &&
-./run_InputValidation.sh &&
-./run_BettingExampleNewScale.sh &&
-./run_BettingExampleNew.sh
+./run_BalanceChecks.sh
+# ./run_InputValidation.sh &&
+# ./run_BettingExampleNewScale.sh &&
+# ./run_BettingExampleNew.sh
