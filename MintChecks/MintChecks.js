5d5d7b8bdb6574937c1f38c945149447d3f96d98
Simon Shine <shreddedglory@gmail.com>
2020-12-12 15:42:56 +0100

    Add burn tests to MintChecks

--- MintChecks/MintChecks.js
+++ MintChecks/MintChecks.js
@@ -47,3 +47,41 @@ assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 200, "DC Settleme
 
 fail(do_mint(contract, 1), "Disallow minting that would exceeed approval");
 fail(do_mint(contract, 0), "Disallow minting of 0");
+
+// Test burning
+
+fail(do_burn(contract, 0), "Disallow burning 0 PT even when some are available");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 10, "PartyToken 0 balance unaffected by failed zero-burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 10, "PartyToken 1 balance unaffected by failed zero-burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 19800, "Settlement asset unaffected by failed zero-burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 200, "DC Settlement unaffected by failed zero-burn");
+
+fail(do_burn(contract, 11), "Disallow burning more PT than available");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 10, "PartyToken 0 balance unaffected by failed burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 10, "PartyToken 1 balance unaffected by failed burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 19800, "Settlement asset unaffected by failed burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 200, "DC Settlement unaffected by failed burn");
+
+succ(do_burn(contract, 5), "Burn some party tokens");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 5, "PartyToken 0 balance is lower after 1st burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 5, "PartyToken 1 balance is lower after 1st burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 19900, "Settlement asset is higher after 1st burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 100, "DC Settlement is lower after 1st burn");
+
+fail(do_burn(contract, 6), "Disallow burning more PT than available");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 5, "PartyToken 0 balance unaffected by failed 2nd burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 5, "PartyToken 1 balance unaffected by failed 2nd burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 19900, "Settlement asset unaffected by failed 2nd burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 100, "DC Settlement unaffected by failed 2nd burn");
+
+succ(do_burn(contract, 5), "Burn the last party tokens");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "PartyToken 0 balance is lower after 2nd (full) burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 0, "PartyToken 1 balance is lower after 2nd (full) burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 20000, "Settlement asset is higher after 2nd (full) burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 0, "DC Settlement is lower after 2nd (full) burn");
+
+fail(do_burn(contract, 0), "Disallow burning 0 PT even when there are 0 PTs");
+assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "PartyToken 0 balance unaffected by failed zero-burn");
+assertEquals(contract.balanceOf(me, 1).toNumber(), 00, "PartyToken 1 balance unaffected by failed zero-burn");
+assertEquals(Erc20_CHF.balanceOf(me).toNumber(), 20000, "Settlement asset unaffected by failed zero-burn");
+assertEquals(Erc20_CHF.balanceOf(contract.address).toNumber(), 0, "DC Settlement unaffected by failed zero-burn");
