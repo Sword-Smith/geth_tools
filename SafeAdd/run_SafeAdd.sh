0878e7a8e3cd3cc0aa048dddb5b093b5da984cc5
Thorkil Vaerge <thorkilk@gmail.com>
2020-12-12 22:13:13 +0100

    Make SafeAdd test less noisy

--- SafeAdd/run_SafeAdd.sh
+++ SafeAdd/run_SafeAdd.sh
@@ -2,10 +2,10 @@
 set -e # HALT on any fail
 
 rm -f out/contracts.json
-./compile_and_deploy.pl out example/Erc20.sol "'SettledAsset'" "'CHF'" 0 20000
-./compile_and_deploy.pl out example/DataFeed0.sol
-./compile_and_deploy.pl out example/DataFeed1.sol
-./compile_and_deploy.pl out SafeAdd/SafeAdd.dag
+./compile_and_deploy.pl out example/Erc20.sol "'SettledAsset'" "'CHF'" 0 20000 > /dev/null
+./compile_and_deploy.pl out example/DataFeed0.sol > /dev/null
+./compile_and_deploy.pl out example/DataFeed1.sol > /dev/null
+./compile_and_deploy.pl out SafeAdd/SafeAdd.dag > /dev/null
 # prepend lib to every test-set
 cat lib.js SafeAdd/SafeAdd.js > out/test.js
 ./run_code.pl out out/test.js
