2f4535b4d12b165e15e2712b306fa73b8d7a6bd2
Simon Shine <shreddedglory@gmail.com>
2020-12-13 17:08:15 +0100

    TransferChecks: Add tests of safeTransferFrom/approvalForAll

--- TransferChecks/TransferChecks.js
+++ TransferChecks/TransferChecks.js
@@ -2,6 +2,8 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 me = web3.eth.accounts[0];
 other = web3.eth.accounts[1];
 
+web3.eth.sendTransaction({to:other, from:me, value:web3.toWei("0.5", "ether")});
+
 var contract = TransferChecks_;
 var contract_address = contract.address;
 log_big("TransferChecks_ test.");
@@ -24,7 +26,7 @@ assertEquals(a1New.toNumber(), 5, "PartyToken 1 should have balance 5 after acti
 assertEquals(saBalanceAfter.toNumber(), 19800, "Settlement asset should have balance 19,800 after activation");
 
 // ...
-do_transfer(contract, me, other, 0, 4);
+succ(do_transfer(contract, me, other, 0, 4), "Transfer from me to other below balance works");
 
 assertEquals(contract.balanceOf(me, 0).toNumber(), 1, "I should have transferred 4 of PT 0 to other.");
 assertEquals(contract.balanceOf(me, 1).toNumber(), 5, "I should not have transferred any of PT 1.");
@@ -50,7 +52,7 @@ assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other's PT0 balance is
 assertEquals(contract.balanceOf(other, 1).toNumber(), 4, "Other's PT1 balance is 4 after failed transfers.");
 
 // Transfer to self should be a nop
-succ(do_transfer(contract, me, me, 0, 1));
+succ(do_transfer(contract, me, me, 0, 1), "Transfer 1 of PT1 to oneself should be a NOP.");
 assertEquals(contract.balanceOf(me, 0).toNumber(), 1, "Transferring PT0 to self is NOP for PT0 balance.");
 assertEquals(contract.balanceOf(me, 1).toNumber(), 1, "Transferring PT0 to self is NOP for PT1 balance.");
 assertEquals(contract.balanceOf(other, 0).toNumber(), 4, "Other's PT0 balance is unaffected by NOP.");
@@ -75,3 +77,39 @@ assertEquals(contract.balanceOf(me, 0).toNumber(), 0, "Own PT0 balance still emp
 assertEquals(contract.balanceOf(me, 1).toNumber(), 0, "Own PT1 balance still empty.");
 assertEquals(contract.balanceOf(other, 0).toNumber(), 5, "Other's PT0 balance is unaffected by failed transfer.");
 assertEquals(contract.balanceOf(other, 1).toNumber(), 5, "Other's PT1 balance is unaffected by failed transfer");
+
+// setApprovalForAllABI
+
+var partyToken0 = 0;
+succ(do_implicit_transfer(contract, other, me, partyToken0, 1, other), "Other can send me 1 of PT 0.");
+assertEquals(contract.balanceOf(me, partyToken0).toNumber(), 1, "Own PT0 balance now up to 1.");
+assertEquals(contract.balanceOf(other, partyToken0).toNumber(), 4, "Other PT1 balance now down to 4.");
+
+fail(do_implicit_transfer(contract, other, me, partyToken0, 1, me), "Caller cannot transfer from someone else without approval.");
+assertEquals(contract.balanceOf(me, partyToken0).toNumber(), 1, "Own PT0 balance still 1.");
+assertEquals(contract.balanceOf(other, partyToken0).toNumber(), 4, "Other PT1 balance still 4.");
+
+fail(do_implicit_transfer(contract, other, me, partyToken0, 0, me), "Caller cannot transfer 0 from someone else without approval.");
+assertEquals(contract.balanceOf(me, partyToken0).toNumber(), 1, "Own PT0 balance still 1.");
+assertEquals(contract.balanceOf(other, partyToken0).toNumber(), 4, "Other PT1 balance still 4.");
+
+// are there other very simple tests?
+
+// function do_setApprovalForAll(contract, operator, approved, caller) {
+
+succ(do_setApprovalForAll(contract, me, true, other), "Other approves me as operator.");
+succ(do_implicit_transfer(contract, other, me, partyToken0, 1, me), "Caller can transfer from someone else when given approval.");
+assertEquals(contract.balanceOf(me, partyToken0).toNumber(), 2, "Own PT0 balance now up to 2.");
+assertEquals(contract.balanceOf(other, partyToken0).toNumber(), 3, "Other PT1 balance now down to 3.");
+
+fail(do_implicit_transfer(contract, me, other, partyToken0, 1, other), "Caller cannot transfer from someone just because caller has approved someone.");
+assertEquals(contract.balanceOf(me, partyToken0).toNumber(), 2, "Own PT0 balance remains 2.");
+assertEquals(contract.balanceOf(other, partyToken0).toNumber(), 3, "Other PT1 balance remains 3.");
+
+succ(do_setApprovalForAll(contract, me, false, other), "Other withdraws approval of me as operator.");
+fail(do_implicit_transfer(contract, other, me, partyToken0, 1, me), "Caller cannot transfer after approval has been revoked.");
+fail(do_implicit_transfer(contract, me, other, partyToken0, 1, other), "Caller cannot transfer after an unrelated approval has been revoked.");
+
+var thirdParty = web3.eth.accounts[2];
+succ(do_setApprovalForAll(contract, thirdParty, true, me), "Approve third-party as operator.");
+fail(do_implicit_transfer(contract, me, other, partyToken0, 1, other), "Verify that approval is restricted to one address.");
