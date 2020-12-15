1162eeadda0358c450946c89807ec1680fe675f3
Thorkil Vaerge <thorkilk@gmail.com>
2020-12-16 00:34:45 +0100

    Reduce STDOUT prints for SafeMul tests

--- SafeMul/run_SafeMul.sh
+++ SafeMul/run_SafeMul.sh
@@ -3,9 +3,9 @@ set -e
 
 rm -f out/contracts.json
 ./compile_and_deploy.pl out example/Erc20.sol "'SettledAsset'" "'CHF'" 0 20000 > /dev/null
-./compile_and_deploy.pl out example/DataFeed0.sol
-./compile_and_deploy.pl out example/DataFeed1.sol
-./compile_and_deploy.pl out example/DataFeed2.sol
-./compile_and_deploy.pl out SafeMul/SafeMul.dag
+./compile_and_deploy.pl out example/DataFeed0.sol > /dev/null
+./compile_and_deploy.pl out example/DataFeed1.sol > /dev/null
+./compile_and_deploy.pl out example/DataFeed2.sol > /dev/null
+./compile_and_deploy.pl out SafeMul/SafeMul.dag > /dev/null
 cat lib.js SafeMul/SafeMul.js > out/test.js
 ./run_code.pl out out/test.js
