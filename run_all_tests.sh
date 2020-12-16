9082ac865ee744887f39ae5d30f88fcf4e92919e
Thorkil Vaerge <thorkilk@gmail.com>
2020-12-16 03:09:48 +0100

    Add input validation test

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -11,6 +11,6 @@ set -e # Halt on error
 ./TransferChecks/run_TransferChecks.sh
 ./BatchTransferChecks/run_BatchTransferChecks.sh
 ./BatchBalanceChecks/run_BatchBalanceChecks.sh
-# ./run_InputValidation.sh &&
+.InputValidation/run_InputValidation.sh
 # ./run_BettingExampleNewScale.sh &&
 # ./run_BettingExampleNew.sh
